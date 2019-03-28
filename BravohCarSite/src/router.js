85888a07669bc9ac670bfcedf391afec6692d52f
Leonardo Fralini <Leonardo Fralini@meucci09.local>
2019-03-28 12:03:23 +0100

    grazie al cazzo

--- BravohCarSite/src/router.js
+++ BravohCarSite/src/router.js
@@ -14,7 +14,7 @@ export default new Router({
     {
       path: "/",
       name: "index",
-      components: { default: Index, header: MainNavbar, footer: MainFooter },
+      components: { default: Landing, header: MainNavbar, footer: MainFooter },
       props: {
         header: { colorOnScroll: 400 },
         footer: { backgroundColor: "black" }
@@ -23,7 +23,7 @@ export default new Router({
     {
       path: "/landing",
       name: "landing",
-      components: { default: Landing, header: MainNavbar, footer: MainFooter },
+      components: { default: Index, header: MainNavbar, footer: MainFooter },
       props: {
         header: { colorOnScroll: 400 },
         footer: { backgroundColor: "black" }
