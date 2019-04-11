fefa99649a20298c59ee6d23d25fae47888aaa74
Leonardo Fralini <Leonardo Fralini@meucci09.local>
2019-04-11 12:47:44 +0200

    adds

--- BravohCarSite/src/methods.js
+++ BravohCarSite/src/methods.js
@@ -1,28 +1,7 @@
-class User {
-
-    singUp() {
-        var values = new FormData();
-        values.append('email', "abc")
-        values.append('password', "cc")
-        fetch('http://localhost:8000/Sign-Up.php', {
-            mode: 'no-cors',
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-form-urlencoded'
-            },
-            body: values,}
-            success: function (response) {
-                console.log(response);
-                // put on console what server sent back...
-            }
+export const getCars = () => {
+        fetch('getCars.php').then((res) => {
+            console.log(res);
         });
+};
 
-    }
-
-}
-
-class Service {
-    getCars() {
-        return 'A';
-    }
-}
+export default getCars;
