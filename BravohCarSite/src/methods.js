0b3a50a04784b24f95187586cc31efee1fa3d9b6
Leonardo Fralini <Leonardo Fralini@meucci09.local>
2019-04-11 11:47:35 +0200

    a

--- BravohCarSite/src/methods.js
+++ BravohCarSite/src/methods.js
@@ -1,17 +1,28 @@
-function logIn() {
-    var values = new FormData();
-    values.append('email', "abc")
-    values.append('password', "cc")
-    fetch('http://localhost:8000/Sign-Up.php', {
-        mode: 'no-cors',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-form-urlencoded'
-        },
-        body: values,
-        success: function (response) {
-            console.log(response);
-            // put on console what server sent back...
-        }
-    });
+class User {
+
+    singUp() {
+        var values = new FormData();
+        values.append('email', "abc")
+        values.append('password', "cc")
+        fetch('http://localhost:8000/Sign-Up.php', {
+            mode: 'no-cors',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-form-urlencoded'
+            },
+            body: values,}
+            success: function (response) {
+                console.log(response);
+                // put on console what server sent back...
+            }
+        });
+
+    }
+
+}
+
+class Service {
+    getCars() {
+        return 'A';
+    }
 }
